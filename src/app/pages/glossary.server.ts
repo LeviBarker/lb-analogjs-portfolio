f76cf3e358e84e368bcf48d475f6eadd86ec54d7
Levi Barker <levi.barker@outlook.com>
2025-12-25 13:34:04 -0600

    more troubleshooting

--- src/app/pages/glossary.server.ts
+++ src/app/pages/glossary.server.ts
@@ -4,26 +4,29 @@ import { GlossaryResponse } from '../services/glossary.service';
 
 export const load = async ({}: PageServerLoad) => {
 
-    let terms: GlossaryResponse | null = null;
-    let error = null;
+    // let terms: GlossaryResponse | null = null;
+    // let error = null;
 
-    let response 
+    // let response 
   
-    try {
-        response = await fetch('https://firestore.googleapis.com/v1/projects/levi-barker-product/databases/(default)/documents/glossary')
-        // terms = await response.json();
-    } catch (e) {
-        error = e;
-    }
+    // try {
+    //     response = await fetch('https://firestore.googleapis.com/v1/projects/levi-barker-product/databases/(default)/documents/glossary')
+    //     // terms = await response.json();
+    // } catch (e) {
+    //     error = e;
+    // }
 
-    if(!response?.ok) {
-        error = `Failed to fetch glossary terms: ${response?.status} ${response?.statusText}`;
-    }
+    // if(!response?.ok) {
+    //     error = `Failed to fetch glossary terms: ${response?.status} ${response?.statusText}`;
+    // }
+
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json());
 
   return {
     // terms,
     response,
     loaded: true,
-    error,
+    error: null,
   };
 };
