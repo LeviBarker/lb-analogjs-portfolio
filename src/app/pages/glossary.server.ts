95eb1b53d423c8e375a1c0f0f4941a82674639c0
Levi Barker <levi.barker@outlook.com>
2025-12-27 18:30:03 -0600

    adds firebase stuff

--- src/app/pages/glossary.server.ts
+++ src/app/pages/glossary.server.ts
@@ -1,8 +1,5 @@
 import { PageServerLoad } from '@analogjs/router';
 
-export const prerender = false;
-export const runtime = 'nodejs';
-
 export const load = async (pageServerLoad: PageServerLoad) => {
   return {
     loaded: true,
