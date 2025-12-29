6b5361a5655f9a0635ccddc08d8e198667f1e843
Levi Barker <levi.barker@outlook.com>
2025-12-29 16:49:57 -0600

    added project and switched from classless picocss version

--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -8,6 +8,13 @@ import { HeroComponent } from '../components/hero.component';
       <h1>Hi, I'm Levi, welcome to my <a href="/glossary?search=semantic">semantic</a>, <a href="/glossary?search=ssr">server-side-rendered</a>, <a href="/glossary?search=meta-framework">meta-framework</a>-powered website.</h1>
       <p>In addition to linking to various projects I've worked on, this site aims to showcase the power of modern web development frameworks and paradigms.</p>
     </app-hero>
+    <h2>Projects</h2>
+    <article>
+      <h3>Pointing Poker</h3>
+      <p>A simple, free, and open-source online planning poker tool for agile teams.  Built with Angular and Firebase.       <a href="https://pointing.levibarker.com" target="_blank" rel="noopener noreferrer">https://pointing.levibarker.com</a>
+</p>
+      <img src="https://firebasestorage.googleapis.com/v0/b/pointing-poker-89f59.appspot.com/o/public%2Fpointing-poker.png?alt=media&token=dd5f93fc-ead0-4b17-a4eb-1e20243df957" />
+    </article>
   `,
   imports: [HeroComponent],
 })
--- src/app/pages/manifest.page.ts
+++ src/app/pages/manifest.page.ts
@@ -1,8 +1,5 @@
-import { Component, computed, effect, inject, signal } from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
-import { map } from 'rxjs';
 import { PageTitleComponent } from '../components/page-title.component';
 
 @Component({
@@ -17,7 +14,6 @@ import { PageTitleComponent } from '../components/page-title.component';
       <li>Analog Framework - <a href="https://analogjs.org" target="_blank" rel="noopener noreferrer">https://analogjs.org</a></li>
       <li>Angular - <a href="https://angular.io" target="_blank" rel="noopener noreferrer">https://angular.io</a></li>
       <li>TypeScript - <a href="https://www.typescriptlang.org" target="_blank" rel="noopener noreferrer">https://www.typescriptlang.org</a></li>
-      <li>RxJS - <a href="https://rxjs.dev" target="_blank" rel="noopener noreferrer">https://rxjs.dev</a></li>
       <li>PicoCSS - <a href="https://picocss.com" target="_blank" rel="noopener noreferrer">https://picocss.com</a></li>
       <li>Vercel - <a href="https://vercel.com" target="_blank" rel="noopener noreferrer">https://vercel.com</a></li>
       <li>GitHub - <a href="https://github.com" target="_blank" rel="noopener noreferrer">https://github.com</a></li>
@@ -28,56 +24,5 @@ import { PageTitleComponent } from '../components/page-title.component';
   imports: [FormsModule, PageTitleComponent],
 })
 export default class Manifest {
-  private readonly route = inject(ActivatedRoute);
-  private readonly searchQueryParam = toSignal(this.route.queryParamMap.pipe(map(params => params.get('search') || '')));
-  protected readonly searchTerm = signal('');
-
-  private readonly entries = [
-    {
-      id: 'meta-framework',
-      term: 'meta-framework',
-      externalLink: 'https://analogjs.org',
-      definition: 'A meta-framework is a higher-level framework built on top of existing frameworks to provide additional features and abstractions.',
-    },
-    {
-      id: 'semantic-html',
-      term: 'semantic HTML',
-      definition: 'Semantic HTML refers to the use of HTML elements that convey meaning and structure, improving accessibility and SEO.',
-    },
-    {
-      id: 'http',
-      term: 'HTTP',
-      definition: 'HTTP (Hypertext Transfer Protocol) is the foundation of data communication on the World Wide Web, enabling the transfer of resources between clients and servers.',
-    },
-    {
-      id: 'server-side-rendering',
-      term: 'server-side rendering (SSR)',
-      definition: 'Server-side rendering (SSR) is the process of generating HTML content on the server and sending it to the client, improving performance and SEO.',
-    },
-    {
-      id: 'cloud-document-store',
-      term: 'cloud document store',
-      definition: 'A cloud document store is a type of NoSQL database that stores data in a flexible, document-oriented format, allowing for easy scalability and accessibility over the internet.',
-    },
-    {
-      id: 'serverless-cloud-function',
-      term: 'serverless cloud function',
-      definition: 'A serverless cloud function is a lightweight, event-driven piece of code that runs in a cloud environment without the need to manage server infrastructure.',
-    }
-  ]
-
-  protected readonly filteredEntries = computed(() => {
-    const term = this.searchTerm().toLowerCase();
-    return this.entries.filter(entry =>
-      entry.term.toLowerCase().includes(term) ||
-      entry.definition.toLowerCase().includes(term)
-    );
-  });
-
-  constructor() {
-    effect(async () => {
-      this.searchTerm.set(this.searchQueryParam() ?? '');
-    });
-  }
 
 }
